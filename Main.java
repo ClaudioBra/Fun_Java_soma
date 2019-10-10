592109396121517c503ec7d10b8e9f3825b9d996
Claudio <55328278+ClaudioBra@users.noreply.github.com>
2019-10-10 11:10:26 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static int soma(int a, int b) {
+        int s = a + b;
+        return s;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int resultado, a, b;
+        char sn = 0;
+        System.out.print("Primeiro valor: ");
+        a = teclado.nextInt();
+        System.out.print("Segundo valor: ");
+        b = teclado.nextInt();
+        resultado = soma(a, b);
+        System.out.println("Soma: " + resultado);
+
+    }
+}
